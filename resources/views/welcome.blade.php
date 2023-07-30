120fb6e9ed0b62430290cb3bfb9fb91250b2d5c8
rkpust <rezaul.cse.pust17@gmail.com>
2023-07-30 14:33:51 +0600

    installed tailwind css and configured

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @vite('resources/css/app.css')
         <title>Dyu</title>
 
         <link rel="shortcut icon" href="{{ asset('image/logo.png') }}">
@@ -14,6 +14,8 @@
         </style>
     </head>
     <body>
-        
+        <div>
+            <h1 class="text-3xl font-bold bg-red-500 mx-auto text-center">Welcome to Dyu Publication</h1>
+        </div>
     </body>
 </html>
